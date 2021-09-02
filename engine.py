66c8f531c89e8dffb5f6fb66c5d852ba26725415
Brenton <brenton.j.andrews@gmail.com>
2021-09-02 12:00:45 -0600

    New Files

---
+++ engine.py
@@ -0,0 +1,50 @@
+"""
+This class stores all information concerning the current game state, such as the board layout and whose
+turn it is. It will also check if winning conditions are present after each turn, check move legality and
+provide a random move generator.
+"""
+
+
+class GameState:
+    def __init__(self):
+        # The game board is represented by a 2d list. The empty strings inside will store X's and O's as players
+        # make moves.
+        self.board = [
+            [" ", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "]
+        ]
+        self.x_to_play = True
+        self.is_won = False
+
+    # Function that checks move legality.
+    def legal(self, move):
+        if self.board[move[0]][move[1]] != ' ':
+            return False
+        else:
+            return True
+
+    # Function checks the current game state for winning conditions.
+    def check_win(self, symbol):
+        # Check columns.
+        col_count = 0
+        for i in range(3):
+            if self.board[i][col_count] == self.board[i][col_count + 1] == self.board[i][col_count + 2] == symbol:
+                print(f"\n{symbol} Wins! Congratulations!")
+                self.is_won = True
+
+        # Check rows.
+        row_count = 0
+        for j in range(3):
+            if self.board[row_count][j] == self.board[row_count + 1][j] == self.board[row_count + 2][j] == symbol:
+                print(f"\n{symbol} Wins! Congratulations!")
+                self.is_won = True
+
+        # Check diagonal / anti-diagonal.
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] == symbol or self.board[2][0] == \
+                self.board[1][1] == self.board[0][2] == symbol:
+            print(f"\n{symbol} Wins! Congratulations!")
+            self.is_won = True
+
+        return False
+
--- main.py
+++ main.py
@@ -1,184 +1,118 @@
-import random
+"""
+The main() file will be our program driver.
+It will handle user / computer input and update command line output / pygame graphics.
+"""
 
-# Board representation strings:
-hor_sep = '-----------'
-x_char = 'X'
-o_char = 'O'
+import pygame as p
+import engine
+
+# Pygame engine init.
+p.init()
+
+# Board Dimension Variables.
+GAME_WIDTH = GAME_HEIGHT = 600
+DIMENSION = 3  # 3 x 3 squares in tic tac toe.
+SQ_SIZE = GAME_HEIGHT / DIMENSION
+# SQ_LOC has a color tuple and screen coordinates for each square printed to the pygame screen.
+SQ_LOC = [((240, 240, 240), 0, 0), ((200, 200, 200), 200, 0), ((240, 240, 240), 400, 0),
+          ((200, 200, 200), 0, 200), ((240, 240, 240), 200, 200), ((200, 200, 200), 400, 200),
+          ((240, 240, 240), 0, 400), ((200, 200, 200), 200, 400), ((240, 240, 240), 400, 400)]
+
+PIECE_LOC = [
+    [(100, 100), (300, 100), (500, 100)],
+    [(100, 300), (300, 300), (500, 200)],
+    [(100, 500), (300, 500), (500, 500)]]
 
 
-# Function that prints board representation in terminal.
-def board_rep(board):
-    print('    1   2   3')
+# Button Constants.
+light_color = (200, 200, 200)
+dark_color = (100, 100, 100)
+choose_symbol = p.font.SysFont('Arial', 80)
+x_select = choose_symbol.render("X", False, (0, 0, 0))
+o_select = choose_symbol.render('O', False, (0, 0, 0))
+
+
+def main():
+    screen = p.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
+    game_state = engine.GameState()
+    screen.fill(p.Color("white"))
+    running = True
+    while running:
+        draw_game_state(screen, game_state)
+        mouse = p.mouse.get_pos()
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            if e.type == p.MOUSEBUTTONDOWN:
+                game_won = game_state_mod(mouse, game_state)
+                if game_won:
+                    running = False
+                    break
+
+        p.display.update()
+        if game_state.is_won:
+            running = False
+
+
+# Function that draws current game state to pygame screen.
+def draw_game_state(screen, game_state):
+    for i in range(9):
+        p.draw.rect(screen, SQ_LOC[i][0], [SQ_LOC[i][1], SQ_LOC[i][2], SQ_SIZE, SQ_SIZE])
+
+    # Render x and o symbols from game state.
     for i in range(3):
-        j = 0
-        print(f'{i + 1}   {board[i][j]} | {board[i][j + 1]} | {board[i][j + 2]} ', end=' ')
-        if i != 2:
-            print(f'\n   {hor_sep}')
-    print("\n")
-
-    return
+        for j in range(3):
+            if game_state.board[i][j] == 'X':
+                screen.blit(x_select, PIECE_LOC[i][j])
+            elif game_state.board[i][j] == 'O':
+                screen.blit(o_select, PIECE_LOC[i][j])
 
 
-# Checks if winning conditions are met.
-def check_win(board, player):
-    # Check columns.
-    col_count = 0
-    for i in range(3):
-        if board[i][col_count] == board[i][col_count + 1] == board[i][col_count + 2] == player:
-            print(f"\n{player} Wins! Congratulations!")
-            return True
+# Function that takes user input and modifies, modifies the game_state and calls winning condition function.
+def game_state_mod(mouse, game_state):
 
-    # Check rows.
-    row_count = 0
-    for j in range(3):
-        if board[row_count][j] == board[row_count + 1][j] == board[row_count + 2][j] == player:
-            print(f"\n{player} Wins! Congratulations!")
-            return True
+    # Check game_state object for player turn.
+    if game_state.x_to_play:
+        symbol = 'X'
+        game_state.x_to_play = False
+    else:
+        symbol = 'O'
+        game_state.x_to_play = True
+    move = []
 
-    # Check diagonal / anti-diagonal.
-    if board[0][0] == board[1][1] == board[2][2] == player or board[2][0] == board[1][1] == board[0][2] == player:
-        print(f"\n{player} Wins! Congratulations!")
-        return True
+    # Find x_position.
+    if mouse[0] < GAME_WIDTH / 3:
+        x_pos = 0
+    elif mouse[0] < GAME_WIDTH / 3 * 2:
+        x_pos = 1
+    elif mouse[0] < GAME_WIDTH:
+        x_pos = 2
+    else:
+        x_pos = None
+
+    # Find y_position.
+    if mouse[1] < GAME_HEIGHT / 3:
+        y_pos = 0
+    elif mouse[1] < GAME_HEIGHT / 3 * 2:
+        y_pos = 1
+    elif mouse[1] < GAME_HEIGHT:
+        y_pos = 2
+    else:
+        y_pos = None
+
+    move = [y_pos, x_pos]
+    print(move)
+    if game_state.legal(move):
+        game_state.board[y_pos][x_pos] = symbol
+        if game_state.check_win(symbol=symbol):
+            print(game_state.board)
+
+    else:
+        game_state.x_to_play = not game_state.x_to_play
+        print(game_state.x_to_play)
 
     return False
 
 
-# Checks that square isn't already occupied.
-def legal(board, func_move):
-    # Check that func_move is valid.
-    if func_move[0] > 3 or func_move[1] > 3:
-        return False
-    if board[func_move[1] - 1][func_move[0] - 1] != ' ':  # Check that grid space is empty.
-        return False
-    else:
-        return True
-
-
-# Player move input.
-def make_move(board, func_count):
-    if func_count % 2 == 0:
-        symbol = x_char
-    else:
-        symbol = o_char
-    legal_move = False
-    while not legal_move:
-        func_move = list(map(int, input(f"{symbol}, Enter your move as 'x y': ").split()))
-        print('\n')
-        if legal(board, func_move):
-            func_count += 1
-            return func_move, func_count
-        else:
-            print("Illegal move, try again.")
-
-    return
-
-
-# Easy mode computer 'engine' -> Generates random moves.
-def rand_move(board):
-    legal_move = False
-    while not legal_move:
-        comp_move = [random.randint(1, 3), random.randint(1, 3)]
-        if legal(board, comp_move):
-            return comp_move
-
-
-# Game initialization:
-play_mode = input("Type 's' for single player or 't' for two player: ")
-game_on = True
-single_player = False
-
-# Two player script:
-if play_mode == 't':
-    while game_on:
-        print('\n')
-        board_arr = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-        game_won = False
-        move_count = 0
-        moves = 1
-
-        # Game script.
-        while not game_won:
-            board_rep(board_arr)
-            print('\n')
-
-            # X input.
-            move, move_count = make_move(board_arr, func_count=move_count)
-            board_arr[move[1] - 1][move[0] - 1] = 'X'
-            board_rep(board_arr)
-            if check_win(board_arr, 'X'):
-                game_won = True
-                break
-
-            # O input.
-            move, move_count = make_move(board_arr, func_count=move_count)
-            board_arr[move[1] - 1][move[0] - 1] = 'O'
-            board_rep(board_arr)
-            if check_win(board_arr, 'O'):
-                game_won = True
-                break
-
-            # Check for a draw.
-            if moves >= 4:
-                print("Its a draw!")
-                game_won = True
-
-        new_game = input("Play again? (y / n): ")
-        if new_game == 'n':
-            game_on = False
-
-# Single player script:
-if play_mode == 's':
-    diff = input("Choose difficulty ('e', 'h'): ")
-    player_symbol = input("Choose 'X' or 'O': ")
-    if player_symbol == 'X':
-        comp_symbol = 'O'
-    else:
-        comp_symbol = 'X'
-
-    while game_on:
-        print('\n')
-        board_arr = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-        game_won = False
-        moves = 1
-
-        while not game_won:
-            print('\n')
-            board_rep(board_arr)  # board_rep prints current GameState.
-            game_won = False
-            moves = 1
-
-            # X move input.
-            if player_symbol == 'X':
-                move_count = 0
-                move, move_count = make_move(board_arr, func_count=move_count)
-            else:
-                print("X makes a move:\n")
-                move = rand_move(board_arr)
-
-            board_arr[move[1] - 1][move[0] - 1] = 'X'
-            board_rep(board_arr)
-            if check_win(board_arr, 'X'):
-                game_won = True
-                break
-
-            # O move input.
-            if player_symbol == 'O':
-                move_count = 1
-                move, move_count = make_move(board_arr, func_count=move_count)
-            else:
-                print("O makes a move:\n")
-                move = rand_move(board_arr)
-
-            board_arr[move[1] - 1][move[0] - 1] = 'O'
-            if check_win(board_arr, 'O'):
-                game_won = True
-                break
-
-            # Check for a draw.
-            if moves >= 4:
-                print("Its a draw!")
-                game_won = True
-
-        new_game = input("Play again? (y / n): ")
-        if new_game == 'n':
-            game_on = False
+if __name__ == "__main__":
+    main()
